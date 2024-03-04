eca668117d7aba301611d66437d43c8a4bd4e4da
Recep <recepcengiz_96@outlook.com>
2024-03-04 09:06:36 +0300

    dataların eklenmesi ve flatlist eklenmesi yapıldı

--- exercises/newsApp/App.tsx
+++ exercises/newsApp/App.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+
 import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList
+} from 'react-native';
+
+import newsData from "./news_data.json";
+
 
 export default function App(): React.JSX.Element {
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <Text> Merhaba Recep </Text>
+
+      <FlatList
+        data={newsData}
+        renderItem={({item})=> <Text> {item.title} </Text> }
+      />
+
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  }
 });
+
+
