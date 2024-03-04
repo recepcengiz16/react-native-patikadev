d8963c5b417ca09afcfeee4c038e78051f6a8bbc
Recep <recepcengiz_96@outlook.com>
2024-03-04 09:11:18 +0300

    card componenti oluşturuldu

---
+++ exercises/newsApp/src/components/NewsCard/NewsCard.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+export default function NewsCard() {
+  return (
+    <View>
+      <Text>NewsCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
