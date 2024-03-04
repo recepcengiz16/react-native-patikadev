3f1dc7c9e718d6a233b563d70f5ad8493a4297df
Recep <recepcengiz_96@outlook.com>
2024-03-04 09:18:29 +0300

    css ayarlamaları yapıldı

--- exercises/newsApp/src/components/NewsCard/NewsCard.js
+++ exercises/newsApp/src/components/NewsCard/NewsCard.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { Text, View, Image } from 'react-native'
+import React from 'react';
+import styles from "./NewsCard.style";
 
-export default function NewsCard() {
+export default function NewsCard(props) {
   return (
-    <View>
-      <Text>NewsCard</Text>
+    <View style={styles.container} >
+      <Image style={styles.image} />  
+      <Text style={styles.title} >NewsCard</Text>
+      <Text style={styles.description} >NewsCard</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({})
--- exercises/newsApp/src/components/NewsCard/NewsCard.style.js
+++ exercises/newsApp/src/components/NewsCard/NewsCard.style.js
@@ -1,5 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+    container:{},
+    image:{},
+    title:{},
+    description:{},
 })
