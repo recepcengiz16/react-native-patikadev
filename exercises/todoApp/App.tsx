67bbc0089881a0ca0fdbe8ae62054de4c322b093
Recep <recepcengiz_96@outlook.com>
2024-03-06 17:46:04 +0300

    itemlar oluşturuldu

--- exercises/todoApp/App.tsx
+++ exercises/todoApp/App.tsx
@@ -1,23 +1,18 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React, {useState} from 'react';
-import Todo from './src/components/Todo';
+import {StyleSheet, View, TouchableOpacity, Text, FlatList} from 'react-native';
+import React from 'react';
+
 
 export default function App(): React.JSX.Element {
 
-  const [isVisible, setIsVisible] = useState(false);
-  const isVisibleObj= {
-    isVisible,
-    setIsVisible,
-  }
-
   return (
-    <View style={styles.container}>
-      <Todo isVisibleObj={isVisibleObj} />
-      <TouchableOpacity 
-        onPress={() => setIsVisible(true)} 
-        style={styles.buttonAdd}
-      >
-        <Text> Ekle </Text>
+    <View style={styles.container}> 
+
+      <FlatList 
+        
+      />
+
+      <TouchableOpacity>
+        <Text>Ekle</Text>
       </TouchableOpacity>
     </View>
   );
@@ -26,13 +21,7 @@ export default function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 200,
-  },
-  buttonAdd: {
-    backgroundColor: '#F194FF',
-    padding: 10,
-    borderRadius: 15
   },
 })
--- exercises/todoApp/src/components/Todo/Todo.js
+++ exercises/todoApp/src/components/Todo/Todo.js
@@ -1,31 +1,11 @@
-import { Text, View, Modal, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
 import styles from './Todo.styles'
 
-export default function Todo({isVisibleObj}) {
+export default function Todo() {
   return (
     <View>
-       <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isVisibleObj.isVisible}
-        >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <TextInput
-            placeholder='Yapılacaklar..' 
-                style={styles.todo}
-            />
-            <TouchableOpacity 
-                
-                style={styles.button}
-                onPress={()=> {isVisibleObj.setIsVisible(false)}}
-            >
-                <Text>Kaydet</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
+      
     </View>
   )
 }
