272d68718bce603011933280b055f20582fc6ecc
Recep <recepcengiz_96@outlook.com>
2024-03-11 11:05:25 +0300

    router yönlendirmesi yapıldı

--- exercises/shopApplication/App.tsx
+++ exercises/shopApplication/App.tsx
@@ -1,10 +1,11 @@
 import {StyleSheet} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Products from './src/pages/Products';
 import Detail from './src/pages/Detail';
+import Login from './src/pages/Login';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,13 @@ export default function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor:"#64b5f6" }}}>
+      <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+           headerShown: false,
+          }}
+         />
         <Stack.Screen
           name="Products"
           component={Products}
