3f6ebd4132b4e943d95483e2f8836d87cc730fd3
Recep <recepcengiz_96@outlook.com>
2024-03-28 09:20:35 +0300

    giriş ve kayıt ol sayfaları yapıldı

---
+++ exercises/firebaseExample/src/pages/auth/Login/Login.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function Login(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
---
+++ exercises/firebaseExample/src/pages/auth/SignUp/SignUp.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function SignUp(): React.JSX.Element {
+  return (
+    <View>
+      <Text>SignUp</Text>
+    </View>
+  )
+}
