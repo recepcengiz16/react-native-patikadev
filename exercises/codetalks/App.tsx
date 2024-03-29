018df07bdc50cb0c454862b5636c97153dc6571a
Recep <recepcengiz_96@outlook.com>
2024-03-29 12:09:52 +0300

    login sayfası oluşturuldu ve router sayfasına eklendi

--- exercises/codetalks/App.tsx
+++ exercises/codetalks/App.tsx
@@ -1,10 +1,16 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './src/pages/Login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{contentStyle:{backgroundColor:"orange"}}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
