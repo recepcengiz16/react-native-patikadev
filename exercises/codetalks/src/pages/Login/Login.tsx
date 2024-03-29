09fafa7584a48c2d35fd7c10daf40657f9e5fbdd
Recep <recepcengiz_96@outlook.com>
2024-03-29 14:50:04 +0300

    login sayfası oluşturuldu ve ana sayfaya eklendi

---
+++ exercises/codetalks/src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import styles from "./Login.style";
+
+export default function Login(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
